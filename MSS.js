a2ed26fb919b19a5bb17731210d142c48aefc192
Marj T <marjt1@yahoo.com>
2018-09-18 11:26:20 -0700

    Add fonts, sticky header

--- MSS.js
+++ MSS.js
@@ -1,3 +1,18 @@
-const introTop = document.querySelector('#intro-top')
+window.onscroll = () => {
+    fixHeader();
+}
 
-introTop.
+const header = document.querySelector('.rectangle-header')
+
+const tagline = document.querySelector('#tagline')
+
+const sticky = header.offsetTop;
+
+const fixHeader = () => {
+    if(window.pageYOffset > sticky) {
+        header.classList.add('sticky');  
+        tagline.style.display = 'none';
+    } else {
+        header.classList.remove('sticky');
+    }
+}
